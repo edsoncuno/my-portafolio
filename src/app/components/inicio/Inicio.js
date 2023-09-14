d8601ad90771951f502a50a3fec6be49cf018992
edsoncuno1@gmail.com <edsoncuno1@gmail.com>
2023-09-13 20:29:59 -0500

    acabe el inicio, falta acerca de mi

--- src/app/components/inicio/Inicio.js
+++ src/app/components/inicio/Inicio.js
@@ -5,12 +5,27 @@ export default function App() {
     <section id="section_inicio" className={styles.app}>
       <div className={styles.left}>
         <div className={styles.greeting}>
-          <div className={styles.phrase}>
+          <h1 className={styles.phrase}>
             CONSTRUIR SOFTWARE<span className={styles.textCursor}></span>
+          </h1>
+          <div className={styles.name}>
+            ¡Hola! Soy
+            <div className={styles.containerTypingText}>
+              <div className={styles.typingText}>Edson Cuno</div>
+            </div>
           </div>
-          <div className={styles.name}>¡Hola! Soy Edson Cuno</div>
           <div className={styles.description}>
             Analista de Datos y Desarrollador Front-End
+          </div>
+          <div className={styles.links}>
+            <a href={"https://github.com/edsoncuno"} target="_blank">
+              <div className={styles.iconGithub}></div>
+              <div>Github</div>
+            </a>
+            <a>
+              <div className={styles.iconLinkedIn}></div>
+              <div>LinkedIn</div>
+            </a>
           </div>
         </div>
       </div>
