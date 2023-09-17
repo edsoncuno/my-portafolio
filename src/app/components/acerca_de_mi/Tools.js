7bd35acd2a83becf480f5c870357d2a9a336add9
edsoncuno1@gmail.com <edsoncuno1@gmail.com>
2023-09-17 14:03:40 -0500

    pruebas de rutas dinamicas 3

--- src/app/components/acerca_de_mi/Tools.js
+++ src/app/components/acerca_de_mi/Tools.js
@@ -4,37 +4,37 @@ import Badges from "./Badges";
 export default function App() {
   const tools = [
     {
-      url: "/../../../../tools/Microsoft_Office_logo.svg",
+      url: "./tools/Microsoft_Office_logo.svg",
       name: "Microsoft Office",
       shadowColor: "#eb3c00",
     },
     {
-      url: "/../../../../tools/New_Power_BI_Logo.svg",
+      url: "./tools/New_Power_BI_Logo.svg",
       name: "Power BI",
       shadowColor: "#e3ad11",
     },
     {
-      url: "/../../../../tools/visual-studio-code-logo-svgrepo-com.svg",
+      url: "./tools/visual-studio-code-logo-svgrepo-com.svg",
       name: "Visual Studio Code",
       shadowColor: "#0179cb",
     },
     {
-      url: "/../../../../tools/ubuntu-svgrepo-com.svg",
+      url: "./tools/ubuntu-svgrepo-com.svg",
       name: "Ubuntu",
       shadowColor: "#e95420",
     },
     {
-      url: "/../../../../tools/microsoft-windows-22-logo-svgrepo-com.svg",
+      url: "./tools/microsoft-windows-22-logo-svgrepo-com.svg",
       name: "Windows",
       shadowColor: "#00adef",
     },
     {
-      url: "/../../../../tools/debian-svgrepo-com.svg",
+      url: "./tools/debian-svgrepo-com.svg",
       name: "Debian",
       shadowColor: "#a80030",
     },
     {
-      url: "/../../../../tools/archlinux-svgrepo-com.svg",
+      url: "./tools/archlinux-svgrepo-com.svg",
       name: "Arch Linux",
       shadowColor: "#1793d1",
     },
